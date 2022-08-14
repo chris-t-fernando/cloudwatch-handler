e44c5c91bbb5c486a2c196052d94c2b7edc298ae
Chris Fernando <chris.t.fernando@gmail.com>
2022-08-14 18:27:29 +1000

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="cloudwatch_handler",
+    version="0.1",
+    description="Shamelessly stolen from https://github.com/ernestomonroy/cloudwatch - I'd have forked but that repo has been deleted",
+    author="Chris Fernando",
+    author_email="chris.t.fernando@gmail.com",
+    url="https://github.com/chris-t-fernando/cloudwatch-handler",
+    #packages=["notification_service"],
+)
