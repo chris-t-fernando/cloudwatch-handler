434a6cc217ca3f17fb4e2be2d6e05d7fc50a7c33
Chris Fernando <chris.t.fernando@gmail.com>
2022-08-16 09:18:59 +1000

    trying module setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from distutils.core import setup
 
 setup(
     name="cloudwatch_handler",
-    version="0.1",
+    version="0.2",
     description="Shamelessly stolen from https://github.com/ernestomonroy/cloudwatch - I'd have forked but that repo has been deleted",
     author="Chris Fernando",
     author_email="chris.t.fernando@gmail.com",
     url="https://github.com/chris-t-fernando/cloudwatch-handler",
-    #packages=["notification_service"],
+    #packages=["cloudwatch_handler"],
+    py_modules = ['cloudwatch_handler.cloudwatch']
 )
